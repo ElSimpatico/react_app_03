88fced4ca4379151ea83460605f5fd02cc250cb5
Aaron Velasco <tiquismiquisss@gmail.com>
2020-03-28 19:41:22 +0100

    feat(Locales): Add locales text
    Closes ç

--- locales/es/common.ts
+++ locales/es/common.ts
@@ -1,5 +1,8 @@
 export const common_es = {
     'welcome-react': 'Welcome to React and react-i18next',
-    driver: 'Conductor',
-    description: 'Descripción'
+    driver: 'Conductor/a',
+    description: 'Descripción',
+    address: 'Dirección',
+    date: 'Fecha',
+    hour: 'Hora'
 };
