0a19e3d13dd14d9a945d92353ad083c6d9b10af6
Aaron Velasco <tiquismiquisss@gmail.com>
2020-03-29 20:45:18 +0200

    test(Jest): Configuration
    Add styles mocks

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
         '^.+\\.tsx?$': 'ts-jest',
         '^.+\\.jsx?$': 'ts-jest'
     },
+    moduleNameMapper: {
+        '\\.(css|less|sass|scss)$': '<rootDir>/test/mocks/styles.ts'
+    },
     setupFiles: ['<rootDir>test/init.ts']
 };
